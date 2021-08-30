2bc9d4c8b565d4fd0c6a844b2225f07e844e23d2
John Alejandro Piedrahita <citymanager02@gmail.com>
2021-08-30 17:44:06 -0500

    refactor: code small inquirer-providers

--- src/ioc/inquirer/inquirer-providers.ts
+++ src/ioc/inquirer/inquirer-providers.ts
@@ -1,9 +1,8 @@
 import { INQUIRER } from './inquirer-constants';
 import {Provider, Scope} from "../../contracts";
 
-const noop = () => {}
 export const inquirerProvider: Provider = {
   provide: INQUIRER,
   scope: Scope.TRANSIENT,
-  useFactory: noop,
+  useFactory: (obj) => obj
 };
