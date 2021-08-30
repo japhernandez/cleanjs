573a84043e53ece315fbeec07b74039f8cc44ecd
John Alejandro Piedrahita <citymanager02@gmail.com>
2021-08-30 18:24:28 -0500

    refactor: code small metadata-scanner

--- src/app/metadata-scanner.ts
+++ src/app/metadata-scanner.ts
@@ -1,39 +1,39 @@
-import { iterate } from 'iterare';
+import {iterate} from 'iterare';
 import {isConstructor, isFunction, isNil} from "../utils";
 import {InjectableInterface} from "../contracts";
 
 
 export class MetadataScanner {
-  public scanFromPrototype<T extends InjectableInterface, R = any>(
-    instance: T,
-    prototype: object,
-    callback: (name: string) => R,
-  ): R[] {
-    const methodNames = new Set(this.getAllFilteredMethodNames(prototype));
-    return iterate(methodNames)
-      .map(callback)
-      .filter(metadata => !isNil(metadata))
-      .toArray();
-  }
+    public scanFromPrototype<T extends InjectableInterface, R = any>(
+        instance: T,
+        prototype: object,
+        callback: (name: string) => R,
+    ): R[] {
+        const methodNames = new Set(this.getAllFilteredMethodNames(prototype));
+        return iterate(methodNames)
+            .map(callback)
+            .filter(metadata => !isNil(metadata))
+            .toArray();
+    }
 
-  *getAllFilteredMethodNames(prototype: object): IterableIterator<string> {
+    * getAllFilteredMethodNames(prototype: object): IterableIterator<string> {
 
-     const isMethod = (prop: string) => {
-      const descriptor = Object.getOwnPropertyDescriptor(prototype, prop);
-      if (descriptor.set || descriptor.get) {
-        return false;
-      }
-      return !isConstructor(prop) && isFunction(prototype[prop]);
-    };
+        const isMethod = (prop: string) => {
+            const descriptor = Object.getOwnPropertyDescriptor(prototype, prop);
+            if (descriptor.set || descriptor.get) {
+                return false;
+            }
+            return !isConstructor(prop) && isFunction(prototype[prop]);
+        };
 
-    console.log('prototype', )
+        console.log('prototype',)
 
-      let type = Reflect.getPrototypeOf(prototype)
+        prototype = Reflect.getPrototypeOf(prototype)
 
-    do {
-      yield* iterate(Object.getOwnPropertyNames(prototype))
-        .filter(isMethod)
-        .toArray();
-    } while ((type) && prototype !== Object.prototype);
-  }
+        do {
+            yield* iterate(Object.getOwnPropertyNames(prototype))
+                .filter(isMethod)
+                .toArray();
+        } while ((prototype) && prototype !== Object.prototype);
+    }
 }
