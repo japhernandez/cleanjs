6147835f6164d467129bea713d3f6834b5959be3
John Alejandro Piedrahita <citymanager02@gmail.com>
2021-08-30 13:38:52 -0500

    refactor: code small pipes-transform

--- src/contracts/pipe-transform.interface.ts
+++ src/contracts/pipe-transform.interface.ts
@@ -19,12 +19,12 @@ export interface ArgumentMetadata {
    * Underlying base type (e.g., `String`) of the parameter, based on the type
    * definition in the route handler.
    */
-  readonly metatype?: Type<any> | undefined;
+  readonly metatype: Type<any> | undefined;
   /**
    * String passed as an argument to the decorator.
    * Example: `@Body('userId')` would yield `userId`
    */
-  readonly data?: string | undefined;
+  readonly data: string | undefined;
 }
 
 /**
