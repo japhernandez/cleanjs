c2f0a42f8adc4e45874ca54b06e36e378cde8bfb
John Alejandro Piedrahita <citymanager02@gmail.com>
2021-08-30 17:41:15 -0500

    refactor: code small request-providers

--- src/routers/request/request-providers.ts
+++ src/routers/request/request-providers.ts
@@ -1,10 +1,8 @@
 import { REQUEST } from './request-constants';
 import {Provider, Scope} from "../../contracts";
 
-// eslint-disable-next-line @typescript-eslint/no-empty-function
-const noop = () => {};
 export const requestProvider: Provider = {
   provide: REQUEST,
   scope: Scope.REQUEST,
-  useFactory: noop,
+  useFactory: (obj) => obj,
 };
