00820d07db6314f5e6b0b7452503a3b9ca7c5e93
John Alejandro Piedrahita <citymanager02@gmail.com>
2021-09-11 00:22:24 -0500

    refactor: refactor package.json

--- src/utils/validate-module-keys.util.ts
+++ src/utils/validate-module-keys.util.ts
@@ -1,23 +1,24 @@
-import { MODULE_METADATA as metadataConstants } from './constants';
+import {MODULE_METADATA as metadataConstants} from './constants';
 
 export const INVALID_MODULE_CONFIG_MESSAGE = (
-  text: TemplateStringsArray,
-  property: string,
+    text: TemplateStringsArray,
+    property: string,
 ) => `Invalid property '${property}' passed into the @Module() decorator.`;
 
 const metadataKeys = [
-  metadataConstants.IMPORTS,
-  metadataConstants.EXPORTS,
-  metadataConstants.CONTROLLERS,
-  metadataConstants.PROVIDERS,
+    metadataConstants.ADAPTERS,
+    metadataConstants.IMPORTS,
+    metadataConstants.EXPORTS,
+    metadataConstants.CONTROLLERS,
+    metadataConstants.PROVIDERS,
 ];
 
 export function validateModuleKeys(keys: string[]) {
-  const validateKey = (key: string) => {
-    if (metadataKeys.includes(key)) {
-      return;
-    }
-    throw new Error(INVALID_MODULE_CONFIG_MESSAGE`${key}`);
-  };
-  keys.forEach(validateKey);
+    const validateKey = (key: string) => {
+        if (metadataKeys.includes(key)) {
+            return;
+        }
+        throw new Error(INVALID_MODULE_CONFIG_MESSAGE`${key}`);
+    };
+    keys.forEach(validateKey);
 }
